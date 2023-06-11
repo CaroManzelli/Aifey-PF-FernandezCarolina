9f96d06e06b72dbf869c5c33635d13997d2ba701
Carolina Fernandez <caromanzelli@gmail.com>
2023-06-11 16:16:14 -0300

    subiendo primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar"
+import ProductsList from "./ProductsList"
+
+function App() {
+
+  return (
+<div>
+  <Navbar />
+  <ProductsList />
+</div>
+
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header>
+      <ul>
+        <li>Inicio</li>
+        <li>Sobre Nosotros</li>
+        <li>Contacto</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
